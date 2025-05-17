5ca7093161c50c4eccd259760db2dcb2792a187f
denobel <denobel@gmail.com>
2025-05-17 12:08:12 +0800

    Initial commit

---
+++ src/main/java/ag/pst/assessment/christiandeleon/ChristiandeleonApplication.java
@@ -0,0 +1,13 @@
+package ag.pst.assessment.christiandeleon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristiandeleonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristiandeleonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ag/pst/assessment/christiandeleon/ChristiandeleonApplicationTests.java
@@ -0,0 +1,13 @@
+package ag.pst.assessment.christiandeleon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristiandeleonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
